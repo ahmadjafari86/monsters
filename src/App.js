5ea8d948163411d7eed0543ae30cdf470a109376
ahmadDev <ahmadjafari6050@gmail.com>
2022-10-14 00:14:24 +0330

    feat: fetch api users

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name:'ahmad',
-      company: 'ztm'
+      monsters: []
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then((response) => response.json())
+    .then((users) => this.setState(
+      () => {
+        return { monsters: users}
+      }, 
+      () => {
+        console.log(this.state);
+      }
+    ));
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={ () => {
-            this.setState(() => {
-              return {name:'Sib'}
-            }, () => {
-              console.log(this.state);
-            })
-          }}>Change name</button>
-        </header>
+        { this.state.monsters.map( monster => <h1 key={monster.id}>{monster.name}</h1>)}
       </div>
     );
   }
