f7d472bf1bbb5188838b51e6a78437910e7fd93f
ahmadDev <ahmadjafari6050@gmail.com>
2022-10-15 00:00:23 +0330

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <SearchBox onChangeHandler={onSearchChange} placeholder = 'Search monster...' className='search-box' />
+        <SearchBox onChangeHandler={onSearchChange} placeholder = 'Search monster...' className='monster-search-box' />
         <CardList monsters={filteredMonster} />
       </div>
     );
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,12 +1,12 @@
 import { Component } from "react";
+import './search-box.styles.css'
 
 class SearchBox extends Component {
     render() { 
         const {onChangeHandler, className, placeholder} = this.props;
         return (
-        <input 
-        className={className} type='search' 
-        placeholder={placeholder} onChange={onChangeHandler} />
+            <input className={`search-box ${className}`} type='search' 
+            placeholder={placeholder} onChange={onChangeHandler} />
         );
     }
 }
