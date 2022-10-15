8a99c4f8d45565921d90de7a8f54d87eedf4cd63
ahmadDev <ahmadjafari6050@gmail.com>
2022-10-15 13:46:49 +0330

    fea: functional components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import { Component } from 'react';
+import { Component, useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    // fetch('https://jsonplaceholder.typicode.com/users/')
+    // .then((response) => response.json())
+    // .then((users) => setMonsters(users));
+    setMonsters([{id:1, name:'sey', email:'sey@gmail.com'}, {id:2, name:'vey', email:'vey@gmail.com'}])
+  }, [])
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    });
+    setFilteredMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
+  
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
+  return (
+    <div className="App">
+    <h1 className='page-title'>Monster Rolodex</h1>
+    <SearchBox onChangeHandler={onSearchChange} placeholder = 'Search monster...' className='monster-search-box' />
+    <CardList monsters={filteredMonsters} />
+  </div>
+  )
+}
+class App1 extends Component {
   constructor() {
     super();
     this.state = {
@@ -34,6 +66,7 @@ class App extends Component {
     });
     return (
       <div className="App">
+        <h1 className='page-title'>Monster Rolodex</h1>
         <SearchBox onChangeHandler={onSearchChange} placeholder = 'Search monster...' className='monster-search-box' />
         <CardList monsters={filteredMonster} />
       </div>
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,13 @@
 import { Component } from "react";
 import './card-list.styles.css'
+import Card from "../card/card.component";
 
 class CardList extends Component {
     render() { 
         const { monsters } = this.props;
         return (
             <div className="card-list">
-                {monsters.map( monster => {
-                    const { id, name, email } = monster;
-                return (
-                    <div className="card-container" key={id}>
-                        <img src={`https://robohash.org/${id}/?set=set2`} alt={`monster ${name}`} />
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>)
-                })}
+                {monsters.map( monster => <Card key={monster.id} monster={monster} />)}
             </div>
         );
     }
